e356ee1d6f77fca125822da137022840e0d6bcf6
Dmirtiy Busygin <Busygin.voll@mail.ru>
2023-08-14 20:16:41 +0300

    task 8 done

--- src/main/java/ru/yandex/tasks/Task8BinarySearch.java
+++ src/main/java/ru/yandex/tasks/Task8BinarySearch.java
@@ -9,14 +9,31 @@ public class Task8BinarySearch {
          * Если таких i несколько, вывести наибольший
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return -1;
+        int l = 0, r = sortedList.length - 1, m;
+        while (l < r) {
+            m = (l + r + 1) / 2;
+            if (sortedList[m] <= number) {
+                l = m;
+            } else {
+                r = m - 1;
+            }
+        }
+        if (sortedList[l] != number) return -1;
+        else return l;
     }
 
     public static void selfCheck() {
         int[] input = {1, 3, 5, 7, 9};
+        int[] input1 = {1, 3, 5, 5, 7, 9};
+        int[] input2 = {1, 3, 5, 5, 7};
+        int[] input3 = {1, 3, 5, 5, 5, 7};
 
         for (int i = 0; i < input.length; i++) {
             assert (find(input[i], input) == i);
         }
+
+        assert (find(input1[2], input1) == 3);
+        assert (find(input2[2], input2) == 3);
+        assert (find(input3[2], input3) == 4);
     }
 }
