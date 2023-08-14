b3d9b09c22b03db9ceb894070e053e147bdef198
Dmirtiy Busygin <Busygin.voll@mail.ru>
2023-08-14 19:34:40 +0300

    task 6 done

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,5 +1,7 @@
 package ru.yandex.tasks;
 
+import java.util.Stack;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
         /*
@@ -7,14 +9,46 @@ public class Task6BraceBalance {
          * Выход: true/false, является ли строка ПСП
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        Stack<Character> stack = new Stack<>();
+        for (char brace : sequence.toCharArray()) {
+            if (brace == '{' || brace == '[' || brace == '(') {
+                stack.push(brace);
+                continue;
+            }
+            if (stack.isEmpty()) return false;
+            if (brace == ')' && stack.peek() == '(') {
+                stack.pop();
+                continue;
+            }
+            if (brace == '}' && stack.peek() == '{') {
+                stack.pop();
+                continue;
+            }
+            if (brace == ']' && stack.peek() == '[') {
+                stack.pop();
+                continue;
+            }
+            return false;
+        }
+
+        return stack.isEmpty();
     }
 
     public static void selfCheck() {
         String test1 = "[({})]{[]}";
         String test2 = "{({})}{";
+        String test3 = "";
+        String test4 = "[";
+        String test5 = "]";
+        String test6 = "[]";
+        String test7 = "{]";
 
         assert checkBalance(test1);
         assert !checkBalance(test2);
+        assert checkBalance(test3);
+        assert !checkBalance(test4);
+        assert !checkBalance(test5);
+        assert checkBalance(test6);
+        assert !checkBalance(test7);
     }
 }
